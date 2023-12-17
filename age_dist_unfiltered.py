2f5d35b83529f25fedb14c6bf636be513d1a0606
bokho <bokhonov7@gmail.com>
2023-12-18 00:24:44 +0400

    major changes

--- age_dist_unfiltered.py
+++ age_dist_unfiltered.py
@@ -5,10 +5,9 @@ from services.filter import filter_dataset
 from services.get_group import age_grouping
 df = pd.read_csv('hh_ru_dataset.csv', sep=',')
 from services.get_resume_df import get_resumes
-df = filter_dataset(df)
 fig, ax = plt.subplots(figsize=(12 , 6))
 fig = sns.kdeplot(color="#FFF465", fill=True, x="year_of_birth", data=df, palette=sns.color_palette(["#FFF465", "#FFF465", "#FFF465", "#FFF465", "#FFF465", "#FFF465"]))
 fig = ax.get_figure()
 plt.ylabel('')
 plt.xlabel("")
-fig.savefig("charts_final/Распределение возрастов фильтр" + '.png', transparent=True)
+fig.savefig("charts_final/Распределение возрастов без фильтра" + '.png', transparent=True)
--- services/correlation_categorical_categorical.py
+++ services/correlation_categorical_categorical.py
@@ -4,5 +4,11 @@ import scipy as sc
 
 
 def correlation_categorical_categorical(categories_1, categories_2):
-    data = pd.crosstab(categories_1, categories_2)
-    return np.sqrt((sc.stats.chi2_contingency(data, correction=True)[0] / data.sum().sum()) / (min(data.shape) - 1))
+    chi2 = sc.stats.chi2_contingency(pd.crosstab(categories_1, categories_2))[0]
+    n = pd.crosstab(categories_1, categories_2).to_numpy().sum()
+    phi2 = chi2/n
+    r, k = pd.crosstab(categories_1, categories_2).shape
+    phi2corr = max(0, phi2 - ((k-1)*(r-1))/(n-1))
+    rcorr = r - ((r-1)**2)/(n-1)
+    kcorr = k - ((k-1)**2)/(n-1)
+    return np.sqrt(phi2corr / min( (kcorr-1), (rcorr-1)))
